056ef5bdb8698a992d657384fd769fd78dfda1b8
Stefan So <s1126025@student.hsleiden.nl>
2023-06-27 19:00:40 +0200

    Added dot

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
         <header className="relative flex flex-col justify-center items-center bg-white h-screen overflow-hidden">
             <section className="relative p-40 z-10">
                 <h2 className="text-white text-2xl lg:text-3xl xl:text-4xl font-semibold drop-shadow-lg mb-5">Hello there, it's Steve</h2>
-                <h1 className="text-white text-6xl lg:text-7xl xl:text-8xl font-bold drop-shadow-lg xl:mr-20">Student, Web designer & Software Engineer</h1>
+                <h1 className="text-white text-6xl lg:text-7xl xl:text-8xl font-bold drop-shadow-lg xl:mr-20">Student, Web designer & Software Engineer.</h1>
             </section>
             <section className="absolute bottom-0 flex justify-between w-full p-10 z-10">
                 <span className="text-white text-sm tracking-wider opacity-50">© Stefan So, All Rights Reserved</span>
